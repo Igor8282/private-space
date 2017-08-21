ab5c9d8c973d0d2fe6411be68834a9041305998b
Pavel Lihanov <lihanov.pavel@danfoss.com>
2017-08-21 09:45:51 +0300

    Changes in models

--- devices/models.py
+++ devices/models.py
@@ -2,20 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-    
 class Document(models.Model):
     url=models.CharField(max_length=255)
-    type_url= models.IntegerField(max_length=2)
+    type_url = models.IntegerField()
 
 class Series(models.Model):
     name=models.CharField(max_length=255)
     documents=models.ForeignKey(Document, on_delete=models.CASCADE, default=None)
 
-class Device (models.Model):
+class Device(models.Model):
     serial_number=models.CharField(max_length=255)
-    series=models.ForeignKey(Series, on_delete=models.CASCADE, default=None)
+    series=models.OneToOneField(Series)
     date=models.DateField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name=models.CharField(max_length=255)    
     
 class Location(models.Model):
@@ -24,9 +23,9 @@ class Location(models.Model):
     devices=models.ForeignKey(Device, on_delete=models.CASCADE, default=None)
     
 class Files(models.Model):
-     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
-     locations=models.ForeignKey(Location, on_delete=models.CASCADE, default=None)
-     devices=models.ForeignKey(Device, on_delete=models.CASCADE, default=None)
+     user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+     location = models.OneToOneField(Location, on_delete=models.CASCADE, default=None)
+     device = models.ForeignKey(Device, on_delete=models.CASCADE, default=None)
 
     
 
--- globals/models.py
+++ globals/models.py
@@ -6,16 +6,13 @@ class Application(models.Model):
     name=models.CharField(max_length=255)
     url=models.CharField(max_length=255)
     
-    
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     organization = models.CharField(max_length=255)
     applications=models.ManyToManyField(Application)
     
-    
-class Notice (models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
-    status=models.IntegerField(max_length=2)
-    applications=models.ManyToManyField(Application)
-    id_content=models.IntegerField(max_length=2)
+class Notification(models.Model):
+    user = models.OneToOneField(User)
+    status=models.IntegerField()
+    application=models.ForeignKey(Application)
     
